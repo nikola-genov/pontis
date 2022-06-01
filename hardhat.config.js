ab5563e504cc44d8dab7702d03ab8725d6995508
Nikola <Nikola@NIKOLATHINKPAD>
2022-06-02 00:28:49 +0300

    More packages and configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,5 +17,25 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/40c2813049e44ec79cb4d7e0d18de173",
+      accounts: [
+        '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
+        '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d'
+      ]
+    },
+    // ropsten: {
+
+    // }
+  }
 };
